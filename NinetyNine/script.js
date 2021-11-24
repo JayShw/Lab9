30e37e33e5f49735cc2b9ecc07ecb87f02f2cb8c
JayShw <92816267+JayShw@users.noreply.github.com>
2021-11-24 00:59:47 +0000

    Add files via upload

---
+++ NinetyNine/script.js
@@ -0,0 +1,13 @@
+function ninety(){
+	for(i=99;i>=0;i--){
+		if(i>1){
+		document.getElementById("main").innerHTML+="<br />"+i+" bottles of beer on the wall, "+i+" bottles of beer, \n Take one down and pass it around, "+(i-1)+" bottles of beer on the wall"
+		//document.createElement('button').
+			}	
+		
+		}
+		document.createElement('button').
+		button.onclick = function MORE(){
+			alert("No more bottles of beer on the wall, no more bottles of beerGo to the store and buy some more, 99 bottles of beer on the wall");
+		}
+	}
